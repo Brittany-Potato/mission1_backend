e417fa9cdd2caecfe6244522e4c6dac628ad47de
potato <Brittanycc@missionreadyhq.com>
2025-05-16 15:22:41 +1200

    Got ever running and all the files organised

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
+//Variables
 const express = require('express');
 const app = express();
 const port = 4000;
 
+const endpoint = process.env.CUSTOM_VISION_ENDPOINT;
+const apikey = process.env.KEY;
+
+//Code
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
+});
+
+app.post(endpoint, (req, res) => {
+    const data = req.body;
+    console.log(data);
+    res.send('Data has been recieved');
 });
